adb3651da6c72f15efe575daee472ff48df8b4f3
CDees <bcd021@latech.edu>
2026-04-11 20:15:07 -0500

    Add custom cursor

--- GameJam/Assets/Scripts/CustomCursor.cs
+++ GameJam/Assets/Scripts/CustomCursor.cs
@@ -2,14 +2,10 @@ using UnityEngine;
 
 public class CustomCursor : MonoBehaviour
 {
+    public Texture2D cursorTexture;
 
     private void Start()
     {
-        
-    }
-
-    private void Update()
-    {
-        
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
     }
 }
