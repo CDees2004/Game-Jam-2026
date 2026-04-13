8625ec727f275be6502ac70ee5bc7ecf783547b5
CDees <bcd021@latech.edu>
2026-04-12 19:19:41 -0500

    Final Push

---
+++ GameJam/Assets/Scripts/Puzzle_BrainUpgrade3.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI; 
+
+public class Puzzle_BrainUpgrade3 : Puzzle
+{
+    public GameObject brainWinButton;
+    public TMP_InputField textField;
+
+    private void Start()
+    {
+        puzzleName = "Brain Upgrade 3";
+        puzzleTimer = 100.0f;
+
+        if (brainWinButton != null)
+        {
+            Button brainWin = brainWinButton.GetComponent<Button>();
+            brainWin.onClick.AddListener(SolvePuzzle);
+        }
+    }
+
+    private void Update()
+    {
+        // timer constantly counting down 
+        puzzleTimer -= Time.deltaTime;
+        if (puzzleTimer <= 0)
+        {
+            FailPuzzle();
+        }
+    }
+
+    public string GetUserInput()
+    {
+        return textField.text;
+    }
+}
